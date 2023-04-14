ab4c7bc914fb0e41c6701a0318d39e636e172f7a
Patricio Perez Cardenal <patricio.perezcardenal@gmail.com>
2023-04-13 21:42:38 -0300

    PRACTICAS LABO_03 - Ejercio 06 [CLASE 01]

---
+++ CLASE_01/dist/Ejercicio_06.js
@@ -0,0 +1,6 @@
+"use strict";
+function alCubo(numero) {
+    return numero * numero * numero;
+}
+console.log(alCubo(3));
+//# sourceMappingURL=Ejercicio_06.js.map
---
+++ CLASE_01/src/Ejercicio_06.ts
@@ -0,0 +1,7 @@
+function alCubo(numero: number)
+{
+    return numero * numero * numero;
+}
+
+console.log(alCubo(3)); // 27
+console.log(alCubo(2)); // 8
